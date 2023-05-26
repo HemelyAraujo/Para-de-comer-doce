bc8062bc5c6f991ae96a85448d728c5acd775391
hemelyAraujo <hemelyraujo21@gmail.com>
2023-05-26 20:08:18 -0300

    execicio resolvido

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int glicemia;
+string diagnostico; 
+ConsoleColor corExibicao;
+
+Console.WriteLine(" -Diabetes Gestacional- \n ");
+
+Console.Write("Glicemia (em mg/dL...:)");
+
+glicemia = Convert.ToInt32(Console.ReadLine());
+
+if (glicemia > 126)
+{
+    diagnostico = "Diabetes na gravidez";
+    corExibicao = ConsoleColor.DarkMagenta;
+}
+else
+{
+    if (glicemia >= 92)
+    {
+        diagnostico = "Compatível com Diabetes Gestacional";
+        corExibicao = ConsoleColor.Blue;
+    }
+    else
+    {
+        diagnostico = "Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose";
+        corExibicao = ConsoleColor.Green;
+    }
+}
+
+Console.ForegroundColor = corExibicao;
+
+Console.WriteLine();
+Console.WriteLine(diagnostico);
+
+Console.ResetColor();
